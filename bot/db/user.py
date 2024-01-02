ef315832ae3984e79c748c25c51668a02d7184f4
rg.batyrshin <rg.batyrshin@localhost>
2024-01-03 02:00:51 +0600

    added sqlalchemy model

---
+++ bot/db/user.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, VARCHAR, DATE
+
+from .base import BaseModel
+
+
+class User(BaseModel):
+    __tablename__ = 'users'
+
+    # Telegram user id
+    user_id = Column(Integer, unique=True, nullable=False)
+
+    # Telegram username
+    username = Column(VARCHAR(32), unique=False, nullable=True)
+
+    # Registration date
+    reg_date = Column(DATE)
+
+    # Last update date
+    upd_date = Column(DATE)
+
+    def __str__(self) -> str:
+        return f"<User:{self.user_id}>"
