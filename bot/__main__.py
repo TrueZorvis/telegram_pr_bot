d8c6497c19240dd9ebecb6648692a137acdc34ca
rg.batyrshin <rg.batyrshin@localhost>
2024-01-03 03:09:08 +0600

    added work with database

--- bot/__main__.py
+++ bot/__main__.py
@@ -3,9 +3,12 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher
 from aiogram.types import BotCommand
+from sqlalchemy.engine import URL
 
 from bot.commands import register_user_commands
 from bot.commands.bot_commands import bot_commands
+
+from db import BaseModel, create_async_engine, get_session_maker, proceed_schemas
 
 
 async def main() -> None:
@@ -20,6 +23,19 @@ async def main() -> None:
     await bot.set_my_commands(commands=commands_for_bot)
 
     register_user_commands(dp)
+
+    postgres_url = URL.create(
+        "postgresql+asyncpg",
+        username=os.getenv('db_user'),
+        password=os.getenv('db_pass'),
+        host='localhost',
+        port=os.getenv('db_port'),
+        database=os.getenv('db_name')
+    )
+
+    async_engine = create_async_engine(postgres_url)
+    session_maker = get_session_maker(async_engine)
+    await proceed_schemas(async_engine, BaseModel.metadata)
 
     await dp.start_polling(bot)
 
--- bot/db/user.py
+++ bot/db/user.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from sqlalchemy import Column, Integer, VARCHAR, DATE
 
 from .base import BaseModel
@@ -7,16 +9,16 @@ class User(BaseModel):
     __tablename__ = 'users'
 
     # Telegram user id
-    user_id = Column(Integer, unique=True, nullable=False)
+    user_id = Column(Integer, unique=True, nullable=False, primary_key=True)
 
     # Telegram username
     username = Column(VARCHAR(32), unique=False, nullable=True)
 
     # Registration date
-    reg_date = Column(DATE)
+    reg_date = Column(DATE, default=date.today())
 
     # Last update date
-    upd_date = Column(DATE)
+    upd_date = Column(DATE, onupdate=date.today())
 
     def __str__(self) -> str:
         return f"<User:{self.user_id}>"
